291e508978c81fee056ac93c2f374a36043bf645
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-25 15:27:11 +0100

    clarifying comment added.

--- server/src/types/posts.types.ts
+++ server/src/types/posts.types.ts
@@ -4,6 +4,7 @@ export type User = {
 	creationDate: Date;
 };
 
+// Sett  to undefined to manage the slightly dodgy way we're handling the post storage for this assignment
 export type Post = {
 	id: string;
 	title: string;
